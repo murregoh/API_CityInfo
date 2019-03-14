e74520441626614bcfeeae7fba3f18757b2e0c28
mateo <mateo.urregoh@gmail.com>
2019-03-13 19:18:45 -0500

    Adding the creation, update and delete methods

--- CitiesInfo/Controllers/PointOfInterestController.cs
+++ CitiesInfo/Controllers/PointOfInterestController.cs
@@ -3,7 +3,7 @@ using CitiesInfo.Models;
 using CitiesInfo.DataStorage;
 using System.Linq;
 using System.Collections.Generic;
-
+using Microsoft.AspNetCore.JsonPatch;
 
 namespace CitiesInfo.Controllers
 {
@@ -37,11 +37,16 @@ namespace CitiesInfo.Controllers
         }
 
         [HttpPost("{cityId}/pointofinterest")]
-        public IActionResult CreatePointOfInterest(int cityId, [FromForm] PointOfInteresForCreationDto pointofinterest) 
+        public IActionResult CreatePointOfInterest(int cityId, [FromBody] PointOfInteresForCreationDto pointOfInterest) 
         {
-            if (pointofinterest == null || !ModelState.IsValid)
+            if (pointOfInterest == null || !ModelState.IsValid)
             {
-                return BadRequest();
+                return BadRequest(ModelState);
+            }
+
+            if (pointOfInterest.name == pointOfInterest.description)
+            {
+                ModelState.AddModelError("Description", "The provide values should not be equal.");
             }
 
             CityDto city = CityDataStorage.Currect.Cities.FirstOrDefault(c => c.id == cityId);
@@ -51,19 +56,112 @@ namespace CitiesInfo.Controllers
                 return NotFound();
             }
 
-            int maxPointsOfInterest = city.pointofinterestdto.Count;
+            int maxPointsOfInterest = city.pointofinterestdto.Max(p => p.id);
 
-            PointOfInterestDto finalPointOfInterest = new PointOfInterestDto() 
+            PointOfInterestDto finalPointOfInterest = new PointOfInterestDto()
             {
                 id = ++maxPointsOfInterest,
-                name = pointofinterest.name,
-                description = pointofinterest.description
+                name = pointOfInterest.name,
+                description = pointOfInterest.description
             };
 
             city.pointofinterestdto.Add(finalPointOfInterest);
 
-            return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, finalPointOfInterest.id }, finalPointOfInterest);
+            return CreatedAtRoute("GetPointOfInterest", new { cityId = cityId, point = finalPointOfInterest.id }, finalPointOfInterest);
 
+        }
+
+        [HttpPut("{cityId}/pointofinterest/{point}")]
+        public IActionResult UpdatePointOfInterest(int cityId, int point, [FromBody] PointOfInterestForUpdateDto pointOfInterest)
+        {
+            if (pointOfInterest == null || !ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (pointOfInterest.name == pointOfInterest.description)
+            {
+                ModelState.AddModelError("Description", "The provide values should not be equal.");
+            }
+
+            CityDto city = CityDataStorage.Currect.Cities.FirstOrDefault(c => c.id == cityId);
+            PointOfInterestDto pointToModify = city.pointofinterestdto.FirstOrDefault(p => p.id == point);
+
+            if (city == null || pointToModify == null)
+            {
+                return NotFound();
+            }
+
+            pointToModify.name = pointOfInterest.name;
+            pointToModify.description = pointOfInterest.description;
+
+            return NoContent();
+
+
+        }
+    
+        [HttpPatch("{cityId}/pointofinterest/{point}")]
+        public IActionResult PartiallyUpdatePointOfInterest(int cityId, int point, [FromBody] JsonPatchDocument<PointOfInterestForUpdateDto> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+
+            CityDto city = CityDataStorage.Currect.Cities.FirstOrDefault(c => c.id == cityId);
+            PointOfInterestDto pointOfInterest = city.pointofinterestdto.FirstOrDefault(p => p.id == point);
+
+            if (city == null || pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestToPatch = new PointOfInterestForUpdateDto()
+            {
+                name = pointOfInterest.name,
+                description = pointOfInterest.description
+            };
+
+            patchDoc.ApplyTo(pointOfInterestToPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (pointOfInterestToPatch.name == pointOfInterestToPatch.description)
+            {
+                ModelState.AddModelError("Description", "The provide values should not be equal.");
+            }
+
+            TryValidateModel(pointOfInterestToPatch);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            pointOfInterest.name = pointOfInterestToPatch.name;
+            pointOfInterest.description = pointOfInterestToPatch.description;
+
+            return NoContent();
+
+        }
+
+        [HttpDelete("{cityId}/pointofinterest/{point}")]
+        public IActionResult DeletePointOfInterest(int cityId, int point)
+        {
+            CityDto city = CityDataStorage.Currect.Cities.FirstOrDefault(c => c.id == cityId);
+            PointOfInterestDto pointOfInterest = city.pointofinterestdto.FirstOrDefault(p => p.id == point);
+
+            if (city == null || pointOfInterest == null)
+            {
+                return NotFound();
+            }
+
+            city.pointofinterestdto.Remove(pointOfInterest);
+
+            return NoContent();
         }
 
     }
---
+++ CitiesInfo/Models/PointOfInterestForUpdateDto.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace CitiesInfo.Models
+{
+    public class PointOfInterestForUpdateDto
+    {
+        private string _name;
+        private string _description;
+
+        [Required(ErrorMessage = "You need to provide a name value.")]
+        [MaxLength(50)]
+        public string name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        [MaxLength(200, ErrorMessage = "Your description exceed 200 characters.")]
+        public string description
+        {
+            get { return _description; }
+            set { _description = value; }
+        }
+    }
+}
